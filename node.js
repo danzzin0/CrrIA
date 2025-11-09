79da8a96bee4af99bc39db3b930c17211fc251ca
danzzin0 <danzzin0800@gmail.com>
2025-11-09 00:18:02 -0300

    Update authorization token for API request

--- node.js
+++ node.js
@@ -12,7 +12,7 @@ app.post("/gerar", async (req, res) => {
   const r = await fetch("https://api-inference.huggingface.co/models/gpt2", {
     method: "POST",
     headers: {
-      "Authorization": "Bearer SEU_TOKEN_GRATUITO_AQUI",
+      "Authorization": "Bearer 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",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ inputs: prompt }),
